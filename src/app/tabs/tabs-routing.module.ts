ae809f8b2c3a845fcc1beab6851192f43a27783a
NicolasDuret <nicolasduret@outlook.com>
2022-01-29 09:05:52 +0100

    changement chemin

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -7,47 +7,56 @@ const routes: Routes = [
     path: 'tabs',
     component: TabsPage,
     children: [
-      {
-        path: 'scan',
-        loadChildren: () => import('../scan/scan.module').then(m => m.ScanPageModule)
-      },
+      // {
+      //   path: 'scan',
+      //   loadChildren: () => import('../scan/scan.module').then(m => m.ScanPageModule)
+      // },
       {
         path: 'profil',
-        loadChildren: () => import('../Profil/profil.module').then(m => m.profilPageModule)
+        loadChildren: () =>
+          import('../Profil/profil.module').then((m) => m.profilPageModule),
       },
       {
         path: 'liste',
-        loadChildren: () => import('../Liste/liste.module').then(m => m.listePageModule)
+        loadChildren: () =>
+          import('../Liste/liste.module').then((m) => m.listePageModule),
       },
       {
         path: 'connexion',
-        loadChildren: () => import('../Connexion/connexion.module').then(m => m.connexionPageModule)
+        loadChildren: () =>
+          import('../Connexion/connexion.module').then(
+            (m) => m.connexionPageModule
+          ),
       },
       {
         path: 'coupon',
-        loadChildren: () => import('../Coupon/coupon.module').then(m => m.couponPageModule)
+        loadChildren: () =>
+          import('../Coupon/coupon.module').then((m) => m.couponPageModule),
       },
       {
         path: 'creation_compte',
-        loadChildren: () => import('../Creation_Compte/creation_compte.module').then(m => m.creation_comptePageModule)
+        loadChildren: () =>
+          import('../Creation_Compte/creation_compte.module').then(
+            (m) => m.creation_comptePageModule
+          ),
       },
-      
-      {
-        path: 'creation_coupon',
-        loadChildren: () => import('../Creation_coupon/creation_coupon.module').then(m => m.creation_couponPageModule)
-      },
+
+      // {
+      //   path: 'creation_coupon',
+      //   loadChildren: () => import('../Creation_coupon/creation_coupon.module').then(m => m.creation_couponPageModule)
+      // },
       {
         path: '',
         redirectTo: '/tabs/liste',
-        pathMatch: 'full'
-      }
-    ]
+        pathMatch: 'full',
+      },
+    ],
   },
   {
     path: '',
     redirectTo: '/tabs/profil',
-    pathMatch: 'full'
-  }
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
